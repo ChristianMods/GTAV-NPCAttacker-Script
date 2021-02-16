43edf1fa95e224373fb0ab6cfb8b6d2fc2c7c10d
ChrisCodes <68878422+ChristianMods@users.noreply.github.com>
2021-02-16 13:59:50 -0500

    added source code

---
+++ Source/attacker.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace npcAttacker
+{
+    public class attacker : Script
+    {
+        public attacker()
+        {
+            this.Tick += onTick;
+            this.KeyUp += onKeyUp;
+            this.KeyDown += onKeyDown;
+        }
+
+        private void onTick(object sender, EventArgs e) // these arent needed but are included for a gernal template for other mods
+        {
+        }
+
+        private void onKeyUp(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e) // source code :)
+        {
+            if (e.KeyCode == Keys.J)
+            {
+                var npc = World.CreatePed(PedHash.LamarDavis, Game.Player.Character.GetOffsetInWorldCoords(new Vector3(0, 5, 0)));
+                npc.Weapons.Give(WeaponHash.Unarmed, 1, true, true);
+                npc.Task.FightAgainst(Game.Player.Character);
+            }
+        }
+    }
+}
